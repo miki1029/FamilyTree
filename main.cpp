508cf8d6e05c35f2d0069b34913bfe77613e2ac4
miki1029 <miki1029g@gmail.com>
2014-05-21 21:41:11 +0900

    init

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+private:
+    Node* next;
+public:
+    bool hasNext();
+    Node* next();
+};
+
+class DummyNode : public Node {
+private:
+    int key; // 세대의 수
+    Node* parent;
+public:
+    int getKey();
+    void setKey(int k);
+    Node* getParent();
+    void setParent(Node* p);
+};
+
+class PersonNode : public Node {
+private:
+    DummyNode* firstNode;
+    DummyNode* childNode;
+    string name;
+    string born;
+    string passedAway;
+public:
+    string getName();
+    void setName(string n);
+    string getBorn();
+    void setBorn(string n);
+    string getPassedAway();
+};
+
+class Slot {
+private:
+    DummyNode* dummyNode;
+    Slot* next;
+public:
+    bool hasNext();
+    Slot* next();
+};
+
+class Table {
+private:
+    Slot** table;
+    int size;
+public:
+    Slot* get(int k);
+    void set(Slot* v);
+};
