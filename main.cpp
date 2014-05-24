f014fefbdbfeff007b6412f2c677b7aad5cfe0ac
Df.Karuna <saver47@gmail.com>
2014-05-24 23:24:06 +0900

    main에서의 입력에 예외처리 지정

--- main.cpp
+++ main.cpp
@@ -7,14 +7,36 @@ int main(int argc, char** argv)
 {
     FamilyTreeManager manager;
     int menu = 0;
+	string sMenu;
 
 	manager.load();
 
-    while (menu != EXIT_CODE)
+    while (true)
     {
         manager.printMenu();
-        cin >> menu;
+        cin >> sMenu;
         cout << endl;
+
+		if( sMenu.compare("1") == 0 ){
+			manager.addPerson();
+		} else if (sMenu.compare("2") == 0 ){
+			manager.find();
+		} else if (sMenu.compare("3") == 0) {
+			manager.modifyPerson();
+		} else if (sMenu.compare("4") == 0) {
+			manager.show();
+		} else if (sMenu.compare("5") == 0) {
+			manager.findPath();
+		} else if (sMenu.compare("6") == 0) {
+			manager.save();
+		} else if (sMenu.compare("7") == 0) {
+			cout << "프로그램을 종료합니다." << endl;
+			break;
+		} else {
+			
+			cout << sMenu << " : 해당 입력은 올바르지 않은 입력입니다." << endl;
+		}
+		/*
         switch (menu)
         {
         case 1:
@@ -35,9 +57,8 @@ int main(int argc, char** argv)
         case 6:
             manager.save();
             break;
-        }
+        }*/
     }
-    cout << "프로그램을 종료합니다." << endl;
 
 	return 0;
 }
